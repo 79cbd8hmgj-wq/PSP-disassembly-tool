c975b368888baaf387400524458086ec0a0e4f08
79cbd8hmgj-wq <lebrechte@icloud.com>
2026-08-25 22:27:08 -0400

    feat: publish Phase 6A advanced analysis API

--- src/pspdisasm/__init__.py
+++ src/pspdisasm/__init__.py
@@ -1,14 +1,16 @@
 """PSP executable analysis and Allegrex disassembly toolkit."""
 
+from .advanced import analyze_advanced
 from .analyzer import analyze_bytes, analyze_file
 from .disassembler import disassemble_bytes, disassemble_file
 from .decompiler import decompile_project_function
 from .matcher import match_project_function
 from .project import generate_project
 
-__version__ = "0.5.0"
+__version__ = "0.6.0"
 
 __all__ = [
+    "analyze_advanced",
     "analyze_bytes",
     "analyze_file",
     "disassemble_bytes",
